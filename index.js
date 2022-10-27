51e1315a19520f5352d1c6e8b84c4ffdcf6245fa
DontStopPrograming <01dyamond01@gmail.com>
2022-10-26 21:54:08 -0500

    modified sections with effect in process

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let abouts = document.querySelectorAll('.sabout')
+let projects = document.querySelectorAll('.sproject')
 
 function mostrarAbout(){
     let scrollLear = document.documentElement.scrollTop
     for(let i = 0; i < abouts.length; i++){
         let alturaPixel = abouts[i].offsetTop
-    if(alturaPixel - 500 < scrollLear){
-       return abouts[i].style.opacity = 1
-        
-        } 
+    if(alturaPixel  < scrollLear){
+        abouts[i].style.opacity = 1
+        } else if(alturaPixel -1000 < scrollLear){
+        abouts[i].style.opacity = 0
+        abouts[i].classList.add('movedescription')
+        }
     }
 }
+
 
 window.addEventListener('scroll', mostrarAbout)
